a5d55f0d67d3ad64f663cfac0381f70f4d546100
Josh Wu <email": null>
2016-05-04 14:23:52 -0400

    add user validations and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  validates :type, :phase, :email, :password, :phone, :first_name, :last_name, presence: true, on: :create
+  validates :type, :phase, :email, :phone, :first_name, :last_name, presence: true, on: :update
+  validates :email, :phone, uniqueness: true
+  validates :password, length: {minimum: 6}
+  validates :phase, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 3}
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+
   has_secure_password
+
 end
--- db/migrate/20160504162612_create_users.rb
+++ db/migrate/20160504162612_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :type
-      t.integer :phase
-      t.string :email
-      t.string :password_digest
-      t.string :phone
-      t.string :first_name
-      t.string :last_name
+      t.string :type , default: 'Student'
+      t.integer :phase, null: false
+      t.string :email, null: false, unique: true, index: true
+      t.string :password_digest, null: false
+      t.string :phone, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
 
       t.timestamps null: false
     end
