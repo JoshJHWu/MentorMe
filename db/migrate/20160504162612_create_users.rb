fdeebcc75e95ede1011c63dc74e5ad5baec0b787
Josh Wu <email": null>
2016-05-04 12:26:59 -0400

    add rails skeleton

---
+++ db/migrate/20160504162612_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :type
+      t.integer :phase
+      t.string :email
+      t.string :password_digest
+      t.string :phone
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
+    end
+  end
+end
