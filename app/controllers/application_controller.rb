02c1bfb394991ceb8dbacefab02fb0fe17aff984
Josh Wu <email": null>
2016-05-04 18:31:26 -0400

    adds multi user layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout( :choose_layout, except: :root )
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id] && current_user.present?
+  end
+
+  def choose_layout
+    if logged_in?
+      current_user.type.downcase
+    else
+      'application'
+    end
+  end
+
 end
