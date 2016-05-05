c30941f09131dad928b1b953379f7aca7ad45f6f
Jason Allen & Jillian Campbell <pair+jasonallenphotography.jillwc07@devbootcamp.com>
2016-05-05 10:28:25 -0400

    update db schema by executing migration on reviews"

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504162612) do
+ActiveRecord::Schema.define(version: 20160505140028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "type",           null: false
+    t.integer  "rating",         null: false
+    t.text     "body",           null: false
+    t.integer  "appointment_id", null: false
+    t.integer  "user_id",        null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "type",            default: "Student"
