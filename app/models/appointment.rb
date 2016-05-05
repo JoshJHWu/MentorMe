630b8aabb29f5b4f1fc3184a353a2726295fd6c3
Josh Wu <email": null>
2016-05-05 11:52:16 -0400

    adds appointments index

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,7 @@ class Appointment < ActiveRecord::Base
   validates :start_time, :mentor_id, presence: true
 
   belongs_to :user
-
+  belongs_to :mentor, class_name: "User"
+  belongs_to :student, class_name: "User"
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,3 @@
 class Student < User
-
+  has_many :appointments, foreign_key: :student_id
 end
