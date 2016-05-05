2df38a475c81e628e098ce059b18c4eb8fd26581
Jason Allen & Jillian Campbell <pair+jasonallenphotography.jillwc07@devbootcamp.com>
2016-05-05 10:27:37 -0400

    Create Reviews migration"

---
+++ db/migrate/20160505140028_create_reviews.rb
@@ -0,0 +1,15 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :type, null: false
+      t.integer :rating, null: false
+      t.text :body, null: false
+      t.references :appointment, null: false
+      t.references :user, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
+
+
