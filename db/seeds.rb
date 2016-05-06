b05fc76eb2966c08a37441c01bcf6359b610fcc1
Jason Allen <email": null>
2016-05-06 10:57:12 -0400

    Updated seed file for users phone number and email uniqueness, updated appointment show and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,30 @@ User.create(type:"Student",
             phase:2,
             email:'c@d.com',
             password:'123456',
-            phone:'123-456-7890',
+            phone:'903-319-1789',
             first_name:'Saralis',
             last_name:'Rivera')
 
 User.create(type:"Mentor",
             phase:3,
-            email:'a@b.com',
+            email:'e@f.com',
             password:'123456',
-            phone:'123-456-7890',
+            phone:'561-235-1606',
             first_name:'Kevin',
             last_name:'Perkins')
 
 User.create(type:"Student",
             phase:1,
-            email:'a@b.com',
+            email:'g@h.com',
             password:'123456',
-            phone:'123-456-7890',
+            phone:'516-931-6336',
             first_name:'Ryan',
-            last_name:'Schutte')
+            last_name:'S')
 
- Review.create(type:'ReviewOfStudent',
-               rating:1,
-               body:'Foo bar baz',
-               user_id:1 )
+ # Review.create(type:'ReviewOfStudent',
+ #               rating:1,
+ #               body:'Foo bar baz',
+ #               user_id:1 )
 
 
 
